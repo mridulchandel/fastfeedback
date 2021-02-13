da0ea6ff037460fe3f06ef8f2639baf36c5f6392
Mridul Chandel <43166857+mridulchandel@users.noreply.github.com>
2021-02-13 13:04:48 +0530

    Update headline (#2)
    * Added Firebase for Auth
    
    * Chakra UI added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+
+import { AuthProvider } from '../lib/auth';
+
+import theme from '../styles/theme';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={extendTheme(theme)}>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,40 @@
 import Head from 'next/head';
+import Image from 'next/image';
+
+import { useAuth } from '../lib/auth';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+  const auth = useAuth();
+
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Fast Feedback</title>
       </Head>
+      <main>
+        <h1>Fast Feedback</h1>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Fast Feedback Project</h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+        <p>
+          Get started by editing <code>pages/index.js</code>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {auth?.user ? (
+          <button onClick={() => auth.signout()}>Sign Out</button>
+        ) : (
+          <button onClick={() => auth.signinWithGithub()}>Sign In</button>
+        )}
+        <div>{auth?.user?.email}</div>
+        {auth?.user?.photoURL && (
+          <div className={styles.profile_container}>
+            <Image
+              src={auth?.user?.photoURL}
+              alt="Profile"
+              width={500}
+              height={500}
+            />
+          </div>
+        )}
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   );
 }
